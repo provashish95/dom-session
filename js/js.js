c0d5f1836fe3dc5ca988bde0b24b9716ecf8167c
Provashish Roy <provashishroy95@gmail.com>
2022-02-09 23:38:09 +0600

    add extra js file

---
+++ js/js.js
@@ -0,0 +1,11 @@
+document.getElementById('add-btn').addEventListener("click", function() {
+    const inputValue = document.getElementById('input-value').value;
+
+    if (inputValue == '') {
+        alert('empty field');
+    } else {
+        const mainContainer = document.getElementById('content-container');
+        const tableContainer = document.createElement('tr');
+        tableContainer = ``
+    }
+})
